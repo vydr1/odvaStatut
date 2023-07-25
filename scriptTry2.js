d796c19135f0ce745ed766d3132e3fa386979e87
vydr1 <puchkov.vova2002@outlook.com>
2023-07-25 10:12:04 +0300

    complete

--- scriptTry2.js
+++ scriptTry2.js
@@ -1,98 +1,101 @@
-//let email = document.getElementById("email");
-//alert(email.value); 
 
 const form = document.forms["form"];
 const formArr = Array.from(form);
 const validFormArr = [];
-const button = form.elements["button"];
-
-//alert(formArr.length);
 
 formArr.forEach((el) => {
-  if (el.hasAttribute("data-reg")) {
-    el.setAttribute("is-valid", "0");
-    validFormArr.push(el);
-  }
+    if (el.hasAttribute("data-reg")) {
+        el.setAttribute("is-valid", "0");
+        validFormArr.push(el);
+    }
 });
 
 form.addEventListener("input", inputHandler);
 form.addEventListener("submit", formCheck);
 
 function inputHandler({ target }) {
-  if (target.hasAttribute("data-reg")) {
-    inputCheck(target);
-  }
+    if (target.hasAttribute("data-reg")) {
+        inputCheck(target);
+    } 
 }
 
 function inputCheck(el) {
-  const inputValue = el.value;
-  const inputReg = el.getAttribute("data-reg");
-  const reg = new RegExp(inputReg);
-  if (reg.test(inputValue)) {
-    el.setAttribute("is-valid", "1");
-    el.style.border = "2px solid rgb(0, 196, 0)";
-  } else {
-    el.setAttribute("is-valid", "0");
-    el.style.border = "2px solid rgb(255, 0, 0)";
-  }
+    const inputValue = el.value;
+    const inputReg = el.getAttribute("data-reg");
+    const reg = new RegExp(inputReg);
+    if (reg.test(inputValue)) {
+        el.setAttribute("is-valid", "1");
+        el.style.border = "2px solid rgb(0, 196, 0)";
+    } else {
+        el.setAttribute("is-valid", "0");
+        el.style.border = "2px solid rgb(255, 0, 0)";
+    }
 }
 
 function formCheck(e) {
-    //alert("sdsdsds");
-  e.preventDefault();
-  const allValid = [];
-  //alert(validFormArr[0]);
-  validFormArr.forEach((el) => {
-    allValid.push(el.getAttribute("is-valid"));
-  });
-  
-  
-  let validcheck = true;
-  allValid.forEach(element => {
-    if(element == '0')
-      {
-        validcheck = false;
-      }
-  });
+    e.preventDefault();
+    const allValid = [];
+    //alert(validFormArr[0]);
+    validFormArr.forEach((el) => {
+        allValid.push(el.getAttribute("is-valid"));
+    });
 
-  if (!validcheck) {
-    alert("Заполните поля правильно!");
-    return;
-  }
-  formSubmit();
+
+    let validcheck = true;
+    allValid.forEach(element => {
+        if(element == '0')
+        {
+            validcheck = false;
+        }
+   });
+
+    if (!validcheck) 
+    {
+        alert("Заполните поля правильно!");
+        //return;
+    }
+    formSubmit();
 }
 
 async function formSubmit() {
-  const data = serializeForm(form);
-  alert("отправка");
-  const response = await sendData(data);
-  //alert(response());
-  if (response.ok) {
-    let result = await response.json();
-    alert(result.message);
-    formReset();
-  } else {
-    alert("Код ошибки: " + response.status);
-  }
+    const data = serializeForm(form);
+    alert("отправка");
+    const response = await sendData(data);
+    if (response.ok) 
+    {
+        let result = await response.json();
+        if(result.success === false)
+        {
+            let errorsOutput = ''; 
+            for (let key in result.errors) 
+            {
+                let value = result.errors[key];
+                errorsOutput += key + " " + value + "\n";
+            }
+            alert(errorsOutput);
+        }
+        else
+        {
+            alert(result.message);
+        }
+    
+
+    } else
+    {
+        alert("Код ошибки: ");
+    }
 }
 
 function serializeForm(formNode) {
-  return new FormData(form);
+    return new FormData(form);
 }
 
 async function sendData(data) {
 
 
-  return await fetch("sendmail.php", {
-    method: "POST",
-    body: data,
-  });
+    return await fetch("sendmail.php", {
+        method: "POST",
+        body: data,
+    });
 }
 
-function formReset() {
-  form.reset();
-  validFormArr.forEach((el) => {
-    el.setAttribute("is-valid", 0);
-    el.style.border = "none";
-  });
-}
--- sendmail.php
+++ sendmail.php
@@ -7,78 +7,175 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-echo 'im here';
+
+$errors = [];
 
 
-$mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8';
-$mail-> setLanguage('ru', 'phpmailer/languange/');
-
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'mail1send2test3@gmail.com';
-$mail->Password = 'ewuukpelubkgwtdo';
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-$mail->Port = 465;
-
-$mail->setFrom('3sendmailtest12@gmail.com', 'test');
-//$mail->addAddress('3sendmailtest12@gmail.com');               
-
-$mail->isHTML(true);
-$mail->Subject = 'bla bla bla';
-
-//echo var_dump($_POST);
-
-$name = $_POST["name"];
-$email = $_POST["email"];
-
-$mail->addAddress($email);               
-
-$phone = $_POST["phone"];
-$secondName = $_POST["secondName"];
-$addNumber = $_POST["addNumber"];
-$dob = $_POST["dob"];
-$third = $_POST["third"];
-
-//$mail->Body .= 'фыврфоы  ' . $name;
-
-$email_template = "template_mail.html";
-
-$body = file_get_contents($email_template);
-
-$body = str_replace('%name%', $name, $body);
-$body = str_replace('%email%', $email, $body);
-$body = str_replace('%secondName%', $secondName, $body);
-$body = str_replace('%third%', $third, $body);
-$body = str_replace('%phone%', $phone, $body);
-$body = str_replace('%addNumber%', $addNumber, $body);
-$body = str_replace('%dob%', $dob, $body);
-
-
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-   // $body .= "grgggggg";
-echo $body;
-
-$mail->Body = $body;
-
-//echo htmlentities($_POST['dob']);
-echo $mail->Body;
-if(count($_POST) != 0){
-    echo 'yyyyyyy';
+foreach($_POST as $fieldName => $fieldValue)
+{
+    if(empty($fieldValue))
+    {
+        $errors[$fieldName] = 'this field is requaered';
+    } 
 }
-var_dump($_POST);
+
+//$_POST['name'] = clear_data($_POST['name']);
+//$_POST['secondName'] = clear_data($_POST['secondName']);
+
+$errors = validateData($errors);
+
+
+if (!empty($errors))
+{
+    echo json_encode([
+        'success' => false,
+        'errors' => $errors
+    ]);
+    exit();
+}
+
+
+$mail = mailerConfig();
+$mail = mailBodyFill($mail);
+
+
 
 if(!$mail->send()) 
 {
-    $message = "Ошибка отправки";
+    header('Content-Type: application/json');
+    echo json_encode([
+        'message' => 'Ошибка отправки'
+    ]);
 } 
 else 
 {
-    $message = "Данные отправлены!";
+    header('Content-Type: application/json');
+    echo json_encode([
+        'message' => 'отправка прошла успешно!'
+    ]);
 }
-        
+
+
+function validateData($errors)
+{
+    $mailRegExp = '/^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/';
+    $nameRegExp = '/^[А-ЯЁ][а-яё]*$/u';
+    $numberRegExp = '/((\+7|7|8)+([0-9]){10})$/';
+    //var_dump($_POST);
+
+    if(!preg_match($mailRegExp, $_POST["email"]))
+    {
+        $errors = validErrorMessageAd($errors, 'email');
+    }
+
+    if(!preg_match($nameRegExp, $_POST["name"]))
+    {
+        $errors = validErrorMessageAd($errors, 'name');
+    }
+
+    if(!preg_match($nameRegExp, $_POST["secondName"]))
+    {
+        $errors = validErrorMessageAd($errors, 'secondName');
+    }
+
+    if(!preg_match($numberRegExp, $_POST["phone"]))
+    {
+        $errors = validErrorMessageAd($errors, 'phone');
+
+    }
+
+    return $errors;
+    
+}
+
+
+function validErrorMessageAd($errors, $name)
+{
+    if (!isset($errors[$name])) 
+    {
+        $errors[$name] = "validation error";
+    }
+    return $errors;
+}
+
+function clear_data($val)
+{
+    $val = trim($val);
+    $val = stripslashes($val);
+    $val = strip_tags($val);
+    $val = htmlspecialchars($val, ENT_QUOTES);
+
+    return $val;
+}
+
+function mailerConfig()
+{
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail-> setLanguage('ru', 'phpmailer/languange/');
+
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'mail1send2test3@gmail.com';
+    $mail->Password = 'ewuukpelubkgwtdo';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 465;
+
+    $mail->setFrom('3sendmailtest12@gmail.com', 'test');
+    //$mail->addAddress('3sendmailtest12@gmail.com');               
+
+    $mail->isHTML(true);
+    return $mail;
+}
+
+function mailBodyFill($mail)
+{
+    $mail->Subject = 'bla bla bla';
+
+    //echo var_dump($_POST);
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+
+    $mail->addAddress($email);               
+
+    $phone = $_POST["phone"];
+    $secondName = $_POST["secondName"];
+    $addNumber = $_POST["addNumber"];
+    $dob = $_POST["dob"];
+    $third = $_POST["third"];
+    $gender = $_POST["gender"];
+
+    //$mail->Body .= 'фыврфоы  ' . $name;
+
+    $email_template = "template_mail.html";
+
+    $body = file_get_contents($email_template);
+
+    $body = str_replace('%name%', $name, $body);
+    $body = str_replace('%email%', $email, $body);
+    $body = str_replace('%secondName%', $secondName, $body);
+    $body = str_replace('%third%', $third, $body);
+    $body = str_replace('%phone%', $phone, $body);
+    $body = str_replace('%addNumber%', $addNumber, $body);
+    $body = str_replace('%dob%', $dob, $body);
+    if($gender === '1')
+    {
+        $body = str_replace('%gender%', ' Женский', $body);
+    }
+    else if($gender === '2')
+    {
+        $body = str_replace('%gender%', ' Мужской', $body);
+
+    }
+
+
+
+    $mail->Body = $body;
+    return $mail;
+}
+
 //header('Content-type: application/json');
 //echo json_encode(($response));
